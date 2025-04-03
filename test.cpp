806e2673dac72df468e14f89d62a713e5ad45d6c
Jannik1020 <jannik.thiele.official@gmail.com>
2025-04-03 20:50:52 +0200

    ...

--- test.cpp
+++ test.cpp
@@ -23,89 +23,64 @@ PWMA  IN1  IN2  OUT1  OUT2      PWMB  IN3  IN4  OUT1  OUT2
 
 using namespace std;
 
-class Motor{
-    public:
-        int pin_dir;
-        int pin_speed;
+class Motor
+{
+public:
+    int pin_dir;
+    int pin_speed;
 
-        Motor(int pin_in, int pin_pwm):pin_dir(pin_in), pin_speed(pin_pwm){
-            pinMode(pin_dir, OUTPUT);
-            pinMode(pin_speed, OUTPUT);
+    Motor(int pin_in, int pin_pwm) : pin_dir(pin_in), pin_speed(pin_pwm)
+    {
+        pinMode(pin_dir, OUTPUT);
+        pinMode(pin_speed, OUTPUT);
 
-            stop();
-        }
+        stop();
+    }
 
-        ~Motor(){
-            stop();
+    ~Motor()
+    {
+        stop();
 
-            pinMode(pin_dir, INPUT);
-            pinMode(pin_speed, INPUT);
-        }
+        pinMode(pin_dir, INPUT);
+        pinMode(pin_speed, INPUT);
+    }
 
-        void forward() {
-            digitalWrite(pin_speed, HIGH);
-            digitalWrite(pin_dir, HIGH);
-        }
+    void forward()
+    {
+        digitalWrite(pin_speed, HIGH);
+        digitalWrite(pin_dir, HIGH);
+    }
 
-        void backward() {
-            digitalWrite(pin_speed, HIGH);
-            digitalWrite(pin_dir, LOW);
-        }
+    void backward()
+    {
+        digitalWrite(pin_speed, HIGH);
+        digitalWrite(pin_dir, LOW);
+    }
 
-        void brake() {
-            digitalWrite(pin_speed, LOW);
-        }
+    void brake()
+    {
+        digitalWrite(pin_speed, LOW);
+    }
 
-        void stop() {
+    void stop()
+    {
 
-            digitalWrite(pin_speed, LOW);
-            digitalWrite(pin_dir, LOW);
-        }
+        digitalWrite(pin_speed, LOW);
+        digitalWrite(pin_dir, LOW);
+    }
 };
 
-
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[])
+{
 
     wiringPiSetupPinType(WPI_PIN_PHYS);
 
-    Motor left(IN_A, PWM_A);
-    Motor right(IN_B, PWM_B);
+    pinMode(38, OUTPUT);
+    pinMode(36, OUTPUT);
 
-    left.forward();
-    right.forward();
-
-    this_thread::sleep_for(chrono::milliseconds(300));
-
-    left.brake();
-    right.brake();
-
-    this_thread::sleep_for(chrono::milliseconds(100));
-
-    left.backward();
-    right.backward();
-
-    this_thread::sleep_for(chrono::milliseconds(300));
-
-    left.stop();
-    right.stop();
-
-    /*string token = "";
-    while(token != "x"){
-        cin >> token;
-        if(token == "f") {
-            left.forward();
-            right.forward();
-        }
-        else if(token == "b"){
-            left.backward();
-            right.backward();
-        }
-        else if(token == "s") {
-            left.brake();
-            right.brake();
-        }
-    }
-
-    left.stop();
-    right.stop();*/
+    digitalWrite(38, HIGH);
+    digitalWrite(36, HIGH);
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
+    digitalWrite(38, LOW);
+    digitalWrite(36, LOW);
 }

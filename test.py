baf2dd34f91e32d1408f95cb5d602957f9a09e04
Jannik1020 <jannik.thiele.official@gmail.com>
2025-04-03 16:31:57 +0200

    first test script, should simply activate motor

---
+++ test.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+
+IN1 = 23
+IN2 = 24
+PWMA = 25
+
+GPIO.setup((IN1, IN2, PWMA), GPIO.OUT )
+
+GPIO.output(IN1, True)
+GPIO.output(IN2, False)
+GPIO.output(PWMA, True)
+
+time.sleep(5)
+
+
+GPIO.output(IN1, False)
+GPIO.output(IN2, False)
+GPIO.output(PWMA, False)
+
+
+GPIO.cleanup()
